b1f242313da15cf3bec03ce50f2ef27de1483e02
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2021-07-10 02:38:55 -0300

    feat: variables and const

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+var a = 3
+// let anda sendo mais utilizado pois é mais voltada para declaração de variáveis com escopo de bloco
+let b = 4
+
+var a = 30
+ b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+// const tem seus valores constantes, portanto não podem ser alterados
+const c = 5
+// c = 50
+console.log(c);
+
