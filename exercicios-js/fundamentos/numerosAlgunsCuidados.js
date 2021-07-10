ddb30f34439578a0d918083aba48c85d10786a5d
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2021-07-10 02:42:17 -0300

    feat: numbers and some care

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+
+console.log(7 / 0) // = gera infinity
+console.log("10" / 2) // pode ser convertido por tipo numerico = 5
+console.log('3' + 2) // nesse caso o + faz sentido na concatenação, portanto vai juntar o 3 com o 2 = 32
+console.log("Show!" * 2) // = o javascript não entende = Not a Number(NaN)
+console.log(0.1 + 0.7) // imprecisão por números flutuantes
+ // console.log(10.toString()) = Não pode converter sendo literal
+console.log(10.345.toFixed(2))
