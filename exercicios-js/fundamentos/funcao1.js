ce94efc0d4f5f38ce543b69fce016273446ef6c2
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2021-07-13 20:00:56 -0300

    feat: example of functions without and with return

---
+++ exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,18 @@
+// Função sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3) // soma
+imprimirSoma(2) // NaN(Not a Number)
+imprimirSoma(2, 10, 4, 5, 6, 7, 8) // pega os dois primeiros números, soma e ignora o resto
+imprimirSoma() // NaN(Not a Number)
+
+// Função com retorno
+function soma(a, b = 1) {
+    return a + b
+}
+
+console.log(soma(2, 3)) // soma
+console.log(soma(2)) // 3
+console.log(soma()) // NaN
