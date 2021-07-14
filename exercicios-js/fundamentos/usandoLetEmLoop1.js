3daa88f5ae0c48d601d1ba6939746dbc64f209a3
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2021-07-14 03:34:47 -0300

    feat: let declaration and scope types in loop

---
+++ exercicios-js/fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+// console.log('i =', i) // pelo fato do let estar em escopo
+                         // de bloco e não global, vai dar erro: i is not defined
