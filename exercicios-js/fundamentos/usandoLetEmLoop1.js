5546d5e4c1b5f2d6ca7ae06c2399279749bad8da
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2021-07-17 02:46:39 -0300

    feat: let in loop

--- exercicios-js/fundamentos/usandoLetEmLoop1.js
+++ exercicios-js/fundamentos/usandoLetEmLoop1.js
@@ -3,4 +3,4 @@ for (let i = 0; i < 10; i++) {
 }
 
 // console.log('i =', i) // pelo fato do let estar em escopo
-                         // de bloco e não global, vai dar erro: i is not defined
+                      // de bloco e não global, vai dar erro: i is not defined
